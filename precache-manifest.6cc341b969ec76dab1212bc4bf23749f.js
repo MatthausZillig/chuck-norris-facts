2863991bb21714a6497c0970a9d70931f3286156
MatthausZillig <matthauszigillig@hotmail.com>
2019-12-26 14:56:18 -0300

    Updates

---
+++ precache-manifest.6cc341b969ec76dab1212bc4bf23749f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794514835858b1fd87921b56c5b57e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "853f78362abcd498a8cb",
+    "url": "/static/css/3.c3fd117e.chunk.css"
+  },
+  {
+    "revision": "37a3c424d65692897b57",
+    "url": "/static/css/4.0822c98a.chunk.css"
+  },
+  {
+    "revision": "ce4d9c6f711267ec43a1",
+    "url": "/static/css/main.08cf55a8.chunk.css"
+  },
+  {
+    "revision": "9bda4c048f1399159a29",
+    "url": "/static/js/2.3ba41e62.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.3ba41e62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "853f78362abcd498a8cb",
+    "url": "/static/js/3.2568a6ee.chunk.js"
+  },
+  {
+    "revision": "37a3c424d65692897b57",
+    "url": "/static/js/4.0a2d7b98.chunk.js"
+  },
+  {
+    "revision": "ce4d9c6f711267ec43a1",
+    "url": "/static/js/main.a235dac6.chunk.js"
+  },
+  {
+    "revision": "d554be01146c1c6f25e7",
+    "url": "/static/js/runtime-main.6fcea1c8.js"
+  }
+]);
