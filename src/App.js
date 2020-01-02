675d55bbdeb3bc1df355f688125aa76ef83d6db3
MatthausZillig <matthauszigillig@hotmail.com>
2020-01-02 19:18:58 -0300

    adding back-button with icon to header component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Header from './components/header/header.component'
 import Loading from './components/loading/loading.component'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 import CategoryContextProvider from './contexts/categoryContext'
 
+library.add(faArrowLeft)
 const Category = React.lazy(() => import('./pages/category/category.component'))
 const Joke = React.lazy(() => import('./pages/joke/joke.component'))
 
