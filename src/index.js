ab40a72ca3cc83f23465848a8d1289f261a4f184
MatthausZillig <matthauszigillig@hotmail.com>
2020-01-05 21:46:10 -0300

    adding favicon and start config to pwa

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { register } from './serviceWorker'
 import './index.css'
 import App from './App'
+
+register()
 
 ReactDOM.render(
   <BrowserRouter>
